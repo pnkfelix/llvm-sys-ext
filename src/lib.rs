4d60a11362475f781a2d1726caa427395ad338ae
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-09-24 15:55:25 +0200

    removed all llvm-ext extensions traits and types. Updated README to reflect purpose of this library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,7 @@
-#![feature(cstr_to_str)]
-
-extern crate cbox;
 extern crate libc;
-extern crate llvm;
 extern crate llvm_sys;
 
-pub use self::ext_traits::{Builder, BasicBlock, Function};
-pub use self::types::{FunctionPassManager};
-
-mod ext_traits;
 mod ext_impls;
-
-mod types;
 
 pub mod sys {
     pub use ::ext_impls::LLVMVerifyFunctionWithOutput;
